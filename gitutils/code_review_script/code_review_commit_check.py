a97d29037c4b2eec4cabd28d9f3d7afe6a405699
yue.zhang <zhangyue@gmail.com>
2017-10-31 14:26:42 -0700

    test
    lineschanged:131
    encryption:YmSWamGVlo5qaYOUlaBqa5tob5FwmZyTZ2aVY5RoYGZnkWaWgZZqoDw=

--- gitutils/code_review_script/code_review_commit_check.py
+++ gitutils/code_review_script/code_review_commit_check.py
@@ -5,73 +5,42 @@ import subprocess
 import ast
 import json 
 import re
+import utils_code_review as utils 
 
-def get_lines_changed_commit(rev):
-	count = 0 
-	stat = subprocess.check_output("git diff --numstat "+rev+"^ "+rev,shell=True).decode('utf-8').splitlines()
-	for line in stat:
-		count += int(re.search(r'\d+', line).group())
-	return count
-
-def get_git_branch_name():
-	return subprocess.check_output(["git", "rev-parse","--abbrev-ref","HEAD"]).decode('utf-8')
-
-def get_git_remote_revision_hash(branch):
-    return subprocess.check_output(['git', 'rev-parse', 'origin/'+branch]).decode('utf-8')
-
-def get_git_revision_hash():
-    return subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8')
-
-def get_unpushed_commits(remote_sha,local_sha):
-	return subprocess.check_output(["git", "rev-list",remote_sha+".."+local_sha]).decode('utf-8')
-
-def get_total_lines_changed(revs_list):
-	lines_changed = 0
-	for rev in revs_list:
-		line = get_lines_changed_commit(rev)
-		if line is not None:
-			lines_changed = lines_changed + int(line)
-	return lines_changed
 
 def commit_to_Release_alert():
-	alert = """\n
-	Hi,you are committing changes directly on Release branch,
-	a code review is required before push.
-	1.If you haven't install the code review tool Arcanist,please go
-	to : https://cr.houzz.net/w/dev-introduction/workflow/
-	2.Then you can type 'arc diff' command to request a code review.
-	Only after your review is approved then you can push code.
-	3.Finally, you are able to push your commits.\n
+	alert = """
+Hi,you are committing changes directly to Release branch,a code review is required before push.
+1.If you haven't install the code review tool Arcanist,please follow link: https://cr.houzz.net/w/dev-introduction/workflow/ to install it.
+2.Then type 'arc diff' command to request a code review.
+3.After your review is approved then you are able to push code.
 	"""
 	print (alert)
 
 def commit_to_master_alert():
-	alert = """\n
-	Hi,as your total changes of current commit is over 500 lines,
-	a code review is required before push.
-	1.If you haven't install the code review tool Arcanist,please go
-	to : https://cr.houzz.net/w/dev-introduction/workflow/
-	2.Then you can type 'arc diff' command to request a code review.
-	Only after your review is approved then you can push code.
-	3.Finally, you are able to push your commits.\n
+	alert = """
+Hi,as your total changes of current commit is over 500 lines,
+a code review is required before push.
+1.If you haven't install the code review tool Arcanist,please follow link: https://cr.houzz.net/w/dev-introduction/workflow/ to install it.
+2.Then type 'arc diff' command to request a code review.
+3.After your review is approved then you are able to push code.
 	"""
 	print (alert)
 
 if __name__ == '__main__':
-	MAX_LINES  = 100
+	MAX_LINES  = 200
 
-	git_branch = get_git_branch_name()
+	git_branch = utils.get_git_branch_name()
 
 	if "master" in git_branch:
-		local_sha  = get_git_revision_hash()
-		remote_sha = get_git_remote_revision_hash(git_branch.rstrip())
-		revs_list 		= get_unpushed_commits(remote_sha.rstrip(),local_sha.rstrip()).splitlines()
-		lines_changed 	= get_total_lines_changed(revs_list)
-		print (lines_changed)
+		local_sha  = utils.get_git_revision_hash()
+		remote_sha = utils.get_git_remote_revision_hash(git_branch.rstrip())
+		revs_list 		= utils.get_unpushed_commits(remote_sha.rstrip(),local_sha.rstrip()).splitlines()
+		lines_changed 	= utils.get_total_lines_changed(revs_list)
+		print ("lines changed : "+str(lines_changed))
 		if lines_changed >= MAX_LINES:
 			commit_to_master_alert()
 	elif "Release" in git_branch:
-		print("You are trying to commit code at Release branch.")
 		commit_to_Release_alert()
 
 
---
+++ gitutils/code_review_script/utils_code_review.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+# Author:yue.zhang@houzz.com
+import sys
+import subprocess
+import ast
+import json 
+import re
+import datetime
+import base64
+
+def get_git_branch_name():
+	return subprocess.check_output(["git", "rev-parse","--abbrev-ref","HEAD"]).decode('utf-8')
+
+def get_git_remote_revision_hash(branch):
+    return subprocess.check_output(['git', 'rev-parse', 'origin/'+branch]).decode('utf-8')
+
+def get_git_revision_hash():
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8')
+
+def get_unpushed_commits(remote_sha,local_sha):
+	return subprocess.check_output(["git", "rev-list",remote_sha+".."+local_sha]).decode('utf-8')
+
+def get_total_lines_changed(revs_list):
+	lines_changed = 0
+	for rev in revs_list:
+		line = get_lines_changed_commit(rev)
+		if line is not None:
+			lines_changed = lines_changed + int(line)
+	return lines_changed
+
+def get_lines_changed_commit(rev):
+	count = 0 
+	stat = subprocess.check_output("git diff --numstat "+rev+"^ "+rev,shell=True).decode('utf-8').splitlines()
+	for line in stat:
+		count += int(re.search(r'\d+', line).group())
+	return count
+
+def get_commit_message(rev):
+	return subprocess.check_output("git log --format=%B -n 1 "+rev,shell=True).decode('utf-8')
+
+def check_code_reviews_apicall(ids):
+	json = "{\"ids\":"+str(ids)+"}"
+	return subprocess.check_output("echo '"+json+"\' | arc call-conduit --conduit-uri https://cr.houzz.net/ differential.query",shell=True)
+
+def get_review_ids_from_msg(revs_list):
+	ids = []
+	for rev in revs_list:
+		for line in get_commit_message(rev).splitlines():
+			if "Differential Revision:" in line:
+				ID =  line[-4:]
+				ids.append(int(ID))
+	return ids
+
+def edit_last_commit_msg(lines_changed,commit):
+    time = datetime.datetime.now()
+    print(time)
+    enc  = encode(time,commit)
+    ori_msg = get_commit_message(commit)
+    new_msg = ori_msg+"\nlineschanged:"+str(lines_changed)+"\n"+"encryption:"+enc
+    subprocess.check_output("git commit --amend -m \""+new_msg+"\"",shell=True)
+
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc)
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+
+    return "".join(dec)
